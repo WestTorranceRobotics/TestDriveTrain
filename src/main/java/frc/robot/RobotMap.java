4935bb732633729ac2ec42b2ede4f36d4c4f49c5
Pineapple131 <92963579+Pineapple131@users.noreply.github.com>
2022-10-27 17:01:07 -0700

    e

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,4 +3,11 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class RobotMap {
+
+public static class ShooterMap{
+
+public static int shootLeaderCANID = 1;
+public static int shootFollowerCANID = 2;
+
+    }
 }
